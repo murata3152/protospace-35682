7ffb98da7da8000225719efc8017c0903d0d5a65
murata3152 <kntnkmr6@gmail.com>
2021-05-27 17:40:49 +0900

    実装完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "prototypes#index"
-  resources :users
-  resources :prototypes
-  resources :comments, only: [:create]
+  resources :users, only: :show
+  resources :prototypes do
+    resources :comments, only: :create
+  end
 end
